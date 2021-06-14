6e2caa55cddca4558055d0a6f7d96e333deb186d
Denis Smetannikov <smet.denis@gmail.com>
2021-06-14 12:51:26 +0200

    Fix badge URL

--- tests/UtilitySuite/CiReportConverterReadmeTest.php
+++ tests/UtilitySuite/CiReportConverterReadmeTest.php
@@ -91,7 +91,7 @@ class CiReportConverterReadmeTest extends AbstractReadmeTest
         return $this->getPreparedBadge($this->getBadge(
             'CI',
             'https://github.com/__VENDOR_ORIG__/__PACKAGE_ORIG__/actions/workflows/main.yml/badge.svg?branch=master',
-            'https://github.com/__VENDOR_ORIG__/__PACKAGE_ORIG__/actions/workflows/main.yml'
+            'https://github.com/__VENDOR_ORIG__/__PACKAGE_ORIG__/actions/workflows/main.yml?query=branch%3Amaster'
         ));
     }
 
